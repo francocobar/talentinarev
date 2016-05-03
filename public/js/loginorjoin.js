6e755f4e10d2415483a86ee66661df8f1f0bdfa6
[francocobar] <[francoescobar95@yahoo.com]>
2016-05-03 14:46:58 +0700

    firsttime

---
+++ public/js/loginorjoin.js
@@ -0,0 +1,53 @@
+$(function() {
+
+    $("#join input").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            // Prevent spam click and default submit behaviour
+            $("#btnSubmit").attr("disabled", true);
+            event.preventDefault();
+
+            // get values from FORM
+            var email = $("input#email").val();
+            var password = $("input#phone").val();
+            $.ajax({
+                url: "/loginorjoin",
+                type: "POST",
+                data: {
+                    email: email,
+                    password: password
+                },
+                cache: false,
+                success: function() {
+                    return "sukses";
+                };,
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+});
